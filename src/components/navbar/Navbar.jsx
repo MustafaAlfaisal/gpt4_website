8fff62e882b3efee229f8e04122f7f6aa9bbf1ba
Mustafa Afaisal <mustafamfaisal@gmail.com>
2023-06-15 13:45:40 +0300

    Add assets to project

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './navbar.css'
+import {RiMenu3Line, RiCloseLin} from 'react-icons/ri';
+import logo from '../../assets/logo.svg'
+// BEM naming convention -> Block__Element--Modifier
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='gpt4__navbar'>
+      <div className='gpt4__navbar__links'>
+        <div className='gpt4__navbar__links__logo'>
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
   )
 }
 
