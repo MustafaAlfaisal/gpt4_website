6f237a4ecb63c5c9da93707d22e009a64d8a5db9
Mustafa Afaisal <mustafamfaisal@gmail.com>
2023-06-15 12:56:16 +0300

    inital commit

---
+++ src/containers/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './footer.css'
+
+function footer() {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default footer
---
+++ src/containers/whatGPT4/WhatGPT4.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './whatGPT4.css'
+
+function WhatGpt4() {
+  return (
+    <div>WhatGPT4</div>
+  )
+}
+
+export default WhatGpt4
